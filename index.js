45fda95611aac2f0ed1f4f1abc18d09f34ad4c6b
John Mwanyika <mwanyikajohn@outlook.com>
2023-10-29 00:10:16 +0300

    parse request header api

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+
+
+app.get("/api/whoami", (req, res) => {
+  const { ip, rawHeaders } = req;
+  console.log(req)
+  res.json({ ipaddress: ip, language: rawHeaders[7], software: rawHeaders[3] })
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
